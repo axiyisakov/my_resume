13d767a1c85e784893a615418829239c96f9f56c
axiy1218 <122154635+axiy1218@users.noreply.github.com>
2024-02-07 19:54:53 +0500

    -test

--- lib/app/presentation/provider/cv/resume_provider.dart
+++ lib/app/presentation/provider/cv/resume_provider.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_resume/app/domain/usecases/cv/resume/get_resume.dart';
+import 'package:my_resume/core/di/locator.dart';
 
-class ResumeProvider with ChangeNotifier {}
+class ResumeProvider with ChangeNotifier {
+  final GetResume getResume = sl<GetResume>();
+}
--- lib/app/presentation/provider/portfolio/portfolio_provider.dart
+++ lib/app/presentation/provider/portfolio/portfolio_provider.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_resume/app/domain/usecases/cv/portfolio/get_portfolio.dart';
+import 'package:my_resume/core/di/locator.dart';
 
-class PortfolioProvider with ChangeNotifier {}
+class PortfolioProvider with ChangeNotifier {
+  final GetPortfolio getPortfolio = sl<GetPortfolio>();
+}
