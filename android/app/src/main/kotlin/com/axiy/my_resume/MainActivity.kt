b319bb1d6ed436256cfd6cf3b9c8be780397631f
Axmadjon Isaqov <131191067+axiyisakov@users.noreply.github.com>
2024-08-14 15:30:56 +0500

    -test

---
+++ android/app/src/main/kotlin/com/axiy/my_resume/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.axiy.my_resume
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/my_resume/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_resume
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
